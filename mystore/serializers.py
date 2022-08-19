76fa287c74ad744e456f527d2261d5a479351332
rados <radoslav.stoyanov@protonmail.com>
2022-08-19 22:44:16 +0300

    Simplify serializers.

--- mystore/serializers.py
+++ mystore/serializers.py
@@ -2,32 +2,14 @@ from mystore.models import Product, Order
 from rest_framework import serializers
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, max_length=100, allow_blank=True)
-    price = serializers.IntegerField()
-
-    def create(self, validated_data):
-        return Product.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.price = validated_data.get('price', instance.price)
-        instance.save()
-        return instance
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'price']
 
 
-class OrderSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    date = serializers.DateField()
+class OrderSerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True)
-
-    def create(self, validated_data):
-        return Order.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.products = validated_data.get('products', instance.products)
-        instance.date = validated_data.get('date', instance.date)
-        instance.save()
-        return instance
-
+    class Meta:
+        model = Order
+        fields = ['id', 'date', 'products']
