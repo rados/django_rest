47a3d133389eacf7e5b4f1c6f40997cce71fa6cd
rados <radoslav.stoyanov@protonmail.com>
2022-08-30 15:30:49 +0300

    Make title field in product model mandatory.

--- mystore/models.py
+++ mystore/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=100, blank=True, default='')
+    title = models.CharField(max_length=100)
     price = models.IntegerField()
 
 
